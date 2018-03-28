4178bd0962401ce1699495691e8a0b62709911b2
성민 <@192.168.0.121>
2018-03-28 16:14:01 +0900

    list update

--- esm0327/src/com/namiya/model/NamiyaDAO.java
+++ esm0327/src/com/namiya/model/NamiyaDAO.java
@@ -20,36 +20,4 @@ public class NamiyaDAO {
 	public static NamiyaDAO getInstance() {
 		return instance;
 	}
-
-	public NamiyaUserVO login(String id, String password) throws SQLException {
-		NamiyaUserVO vo = null;
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			con = dataSource.getConnection();
-			StringBuilder sql = new StringBuilder();
-			sql.append("SELECT nickname, grade");
-			sql.append("FROM namiya_user");
-			sql.append("WHERE id = ? AND PASSWORD = ?");
-			/*
-			SELECT nickname, grade
-			FROM namiya_user
-			WHERE id = 'test@naver.com' AND PASSWORD = '123'
-			*/
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setString(1, id);
-			pstmt.setString(2, password);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				vo = new NamiyaUserVO();
-				vo.setId(id);
-				vo.setNickName(rs.getString(1));
-				vo.setGrade(rs.getString(2));
-			}
-		} finally {
-			db.closeAll(rs, pstmt, con); 
-		}
-		return vo;
-	}
 }
--- esm0327/src/com/namiya/model/UserDAO.java
+++ esm0327/src/com/namiya/model/UserDAO.java
@@ -1,9 +1,52 @@
 package com.namiya.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 public class UserDAO {
 	private static UserDAO instance=new UserDAO();
-	private UserDAO() {}
+	private DataSourceManager db;
+	private DataSource dataSource;
+	private UserDAO() {
+		db=DataSourceManager.getInstance();
+		dataSource=db.getDataSource();
+	}
 	public static UserDAO getInstance() {
 		return instance;
+	}
+	public NamiyaUserVO login(String id, String password) throws SQLException {
+		NamiyaUserVO vo = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = dataSource.getConnection();
+			StringBuilder sql = new StringBuilder();
+			sql.append("SELECT nickname, grade");
+			sql.append("FROM namiya_user");
+			sql.append("WHERE id = ? AND PASSWORD = ?");
+			/*
+			SELECT nickname, grade
+			FROM namiya_user
+			WHERE id = 'test@naver.com' AND PASSWORD = '123'
+			*/
+			pstmt = con.prepareStatement(sql.toString());
+			pstmt.setString(1, id);
+			pstmt.setString(2, password);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				vo = new NamiyaUserVO();
+				vo.setId(id);
+				vo.setNickName(rs.getString(1));
+				vo.setGrade(rs.getString(2));
+			}
+		} finally {
+			db.closeAll(rs, pstmt, con); 
+		}
+		return vo;
 	}
 }
