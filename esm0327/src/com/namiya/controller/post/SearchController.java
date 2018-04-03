57dc6e6f8e56d7cad3b18b48d4374ee1abd13dbd
wansun <rws4987@naver.com>
2018-04-03 16:29:04 +0900

    검색 페이징 다했당

--- esm0327/src/com/namiya/controller/post/SearchController.java
+++ esm0327/src/com/namiya/controller/post/SearchController.java
@@ -15,8 +15,10 @@ public class SearchController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		//게시글을 검색하기 위한 컨트롤러
-		int totalCount=NamiyaDAO.getInstance().getTotalPostCount();
+		String category=request.getParameter("category");
+		String keyword = request.getParameter("keyword");
 		String nowPage=request.getParameter("pageNo");
+		int totalCount=NamiyaDAO.getInstance().getSearchPostCount(category,keyword);
 		PagingBean pagingBean=null;
 		if(nowPage==null) {
 			pagingBean=new PagingBean(totalCount);
@@ -24,8 +26,6 @@ public class SearchController implements Controller {
 			pagingBean=new PagingBean(totalCount, Integer.parseInt(nowPage));
 		}
 		ArrayList<NamiyaPostVO> list = new ArrayList<>();
-		String category=request.getParameter("category");
-		String keyword = request.getParameter("keyword");
 		if(category.equals("제목")) {
 			list = NamiyaDAO.getInstance().readPostByTitle(keyword, pagingBean);
 		}else if(category.equals("작성자")) {
@@ -34,6 +34,8 @@ public class SearchController implements Controller {
 			list = NamiyaDAO.getInstance().readPostByContent(keyword, pagingBean);
 		}
 		ListVO vo=new ListVO(list, pagingBean);
+		request.setAttribute("category", category);
+		request.setAttribute("keyword", keyword);
 		request.setAttribute("listvo",vo);
 		request.setAttribute("url", "/post/searchList.jsp");
 		return "home.jsp";
